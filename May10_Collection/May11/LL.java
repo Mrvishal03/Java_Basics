3e9bb530e75782ab462a0a476203ead5ca9033f7
Mrvishal03 <mrvishal152207@gmail.com>
2024-05-13 12:10:54 +0530

    NewContent

--- May10_Collection/May11/LL.java
+++ May10_Collection/May11/LL.java
@@ -24,7 +24,7 @@ class additionalFunction {
     }
 
     public Link insertBeg(Link head, int item) {
-        System.out.println("Insert at begining:");
+      
         Link newnode = new Link(item);
         newnode.next = head;
         head = newnode;
@@ -43,7 +43,7 @@ class additionalFunction {
     }
 
     public Link insertAfterGivenLocation(Link head, int item, int data) {
-        System.out.println("Insert after given location:");
+      
 
         Link newnode = new Link(data);
         Link temp = head;
@@ -61,7 +61,7 @@ class additionalFunction {
     }
 
     public Link insertBeforeGivenLocation(Link head, int item, int data) {
-        System.out.println("Insert before specific location");
+        
 
         Link newnode = new Link(data);
         Link temp = head;
@@ -82,7 +82,7 @@ class additionalFunction {
     }
 
     public Link DeleteEnd(Link head) {
-        System.out.println("Delete last node");
+        
         Link prev = null;
         Link temp = head;
         while (temp.next != null) {
@@ -94,6 +94,69 @@ class additionalFunction {
 
     }
 
+    public Link DeleteBegning(Link head)
+    {
+       head = head.next;
+       return head;
+
+    }
+
+    public Link DeleteSpecificElement(Link head ,int item)
+    {
+       Link temp=head;
+       Link prev=null;
+       while(temp!=null)
+       {
+          if (item ==temp.data) {
+            prev.next=temp.next;
+             break;
+          }
+        prev = temp;
+        temp = temp.next;
+       }
+       return head;
+    }
+
+   public Link SwappingTwoNodes (Link head ,int item1,int item2)
+   {
+
+    Link temp1 = head;
+    Link temp2 = head;
+    while (temp1!=null) {
+        if (item1==temp1.data) {
+            break;
+        }
+        temp1 = temp1.next;
+    }
+    while (temp2!=null) {
+        if (item2==temp2.data) {
+            break;
+        }
+        temp2 = temp2.next;
+    }
+    if (temp1!=null && temp2!=null) {
+        int t = temp1.data;
+        temp1.data = temp2.data;
+        temp2.data = t;
+
+    }
+    return head;
+   }
+
+   public Link iterateReverse( Link head){
+    Link curr = head;
+    Link prev= null;
+    while (curr!= null ) {
+        Link temp = curr.next;
+        curr.next = prev;
+         prev = curr;
+         curr = temp;   
+    }
+    return prev;
+   }
+
+
+
 }
 public class LL {
     public static void main(String[] args) {
@@ -102,14 +165,40 @@ public class LL {
         head = obj.insertEnd(head, 11);
         head = obj.insertEnd(head, 22);
         head = obj.insertEnd(head, 33);
+        head = obj.insertEnd(head, 44);
+        head = obj.insertEnd(head, 55);
         obj.display(head);
+
+        System.out.println("Insert at begining:");
         head = obj.insertBeg(head, 88);
         obj.display(head);
+
+        System.out.println("Insert after given location:");
         head = obj.insertAfterGivenLocation(head, 22, 66);
         obj.display(head);
+
+        System.out.println("Insert before specific location");
         head = obj.insertBeforeGivenLocation(head, 22, 66);
         obj.display(head);
+
+        System.out.println("Delete last node");
         head = obj.DeleteEnd(head);
+        obj.display(head);
+
+        System.out.println("Delete 1st node");
+        head=obj.DeleteBegning(head);
+        obj.display(head);
+
+        System.out.println("Delete Specific Location");
+        head=obj.DeleteSpecificElement(head, 22);
+        obj.display(head);
+
+        System.out.println("Swapping Two Nodes ");
+        head = obj.SwappingTwoNodes(head, 11, 44);
+        obj.display(head);
+          
+        System.out.println("Reverse Printing");
+        head=obj.iterateReverse(head);
         obj.display(head);
 
     }
